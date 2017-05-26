f769ccfba24d1995fd1425a4b5918bca79d0e080
BardinPetr <bardin.petr@gmail.com>
2017-05-26 20:09:31 +0300

    Settings finished

--- app.js
+++ app.js
@@ -5,7 +5,9 @@
 const electron = require('electron');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
-const { ipcMain } = require('electron');
+const {
+  ipcMain
+} = require('electron');
 const fs = require('fs');
 var Avrgirl = require('avrgirl-arduino');
 var SerialPort = require("serialport");
@@ -29,52 +31,56 @@ var onlyMainBoards = true;
 var baudR = 9600;
 
 app.on('window-all-closed', function() {
-    if (process.platform !== 'darwin') {
-        app.quit();
-    }
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
 });
 
 app.on('ready', function() {
-    mainWindow = new BrowserWindow({
-        width: 900,
-        height: 1000,
-        resizable: false,
-        frame: false,
-        icon: "app/media/icon64.png",
-        title: "TechLabIDE",
-        show: true
-    });
+  mainWindow = new BrowserWindow({
+    width: 900,
+    height: 1000,
+    resizable: false,
+    frame: false,
+    icon: "app/media/icon64.png",
+    title: "TechLabIDE",
+    show: true
+  });
 
-    mainWindow.loadURL('file://' + __dirname + '/app/index.html');
-    mainWindow.maximize();
+  mainWindow.loadURL('file://' + __dirname + '/app/index.html');
+  mainWindow.maximize();
 
-    if (require("./settings.json").debug) mainWindow.webContents.openDevTools();
+  if (require("./settings.json").debug) mainWindow.webContents.openDevTools();
 
-    mainWindow.on('closed', function() {
-        mainWindow = null;
-    });
+  mainWindow.on('closed', function() {
+    mainWindow = null;
+  });
 
-    termWindow = new BrowserWindow({
-        width: 770,
-        height: 500,
-        minWidth: 770,
-        minHeight: 510,
-        frame: false,
-        show: false
-    });
-    termWindow.loadURL('file://' + __dirname + '/app/term.html');
+  termWindow = new BrowserWindow({
+    width: 770,
+    height: 500,
+    minWidth: 770,
+    minHeight: 510,
+    frame: false,
+    show: false,
+    icon: "app/media/icons/terminal.png",
+    title: "Terminal"
+  });
+  termWindow.loadURL('file://' + __dirname + '/app/term.html');
 
-    setWindow = new BrowserWindow({
-        width: 770,
-        height: 500,
-        minWidth: 770,
-        minHeight: 510,
-        frame: false,
-        show: false
-    });
-    setWindow.loadURL('file://' + __dirname + '/app/set.html');
+  setWindow = new BrowserWindow({
+    width: 770,
+    height: 500,
+    minWidth: 770,
+    minHeight: 510,
+    frame: false,
+    show: false,
+    icon: "app/media/icons/settings.png",
+    title: "Settings"
+  });
+  setWindow.loadURL('file://' + __dirname + '/app/set.html');
 
-    onlyMainBoards = require("./settings.json").onlyMainBoards;
+  onlyMainBoards = require("./settings.json").onlyMainBoards;
 });
 
 
@@ -82,132 +88,142 @@ app.on('ready', function() {
  * Ipc handlers
  */
 ipcMain.on('ready', function(e, d) {
-    global.sender = e.sender;
-    boards = get_boards();
-    global.sender.send('boardsRefresh', boards);
-    board = boards[0];
-    global.sender.send('run', require("./settings.json"));
+  global.sender = e.sender;
+  boards = get_boards();
+  global.sender.send('boardsRefresh', boards);
+  board = boards[0];
+  global.sender.send('run', require("./settings.json"));
 });
 
 
 ipcMain.on('close', function() {
-    app.quit();
+  app.quit();
 });
 ipcMain.on('min', function() {
-    mainWindow.minimize();
+  mainWindow.minimize();
 });
 
 ipcMain.on('Tclose', function(e, d) {
-    e.sender.send("TcloseNow");
-    termWindow.hide();
+  e.sender.send("TcloseNow");
+  termWindow.hide();
 });
 ipcMain.on('Tmin', function() {
-    termWindow.minimize();
+  termWindow.minimize();
 });
 ipcMain.on('Tmax', function() {
-    termWindow.maximize();
+  termWindow.maximize();
 });
 
 ipcMain.on('Sclose', function(e, d) {
-    setWindow.hide();
+  setWindow.hide();
 });
 
 ipcMain.on('compile', function(e, d) {
-    get_hex(d, function(res, c) {
-        global.sender.send("c", (c == 0 ? true : false));
-    });
+  get_hex(d, function(res, c) {
+    global.sender.send("c", (c == 0 ? true : false));
+  });
 });
 ipcMain.on('upload', function(e, d) {
-    if (port === undefined) {
-        e.sender.send("noport");
-        return;
-    } else {
-        e.sender.send("cstart");
-    }
-    get_hex(d, function(res, c) {
-        var avrgirl = new Avrgirl({
-            board: board.name,
-            port: port.comName,
-            debug: true
-        });
-        var fs = require('fs');
-        fs.writeFile("data.hex", res, function(err) {
-            if (err) {
-                log("write file err")
-            } else {
-                avrgirl.flash("data.hex", function(error) {
-                    if (error) {
-                        console.error(error);
-                    } else {
-                        console.info('done.');
-                    }
-                    global.sender.send("u", (error ? false : true));
-                });
-            }
-        });
+  if (port === undefined) {
+    e.sender.send("noport");
+    return;
+  } else {
+    e.sender.send("cstart");
+  }
+  get_hex(d, function(res, c) {
+    var avrgirl = new Avrgirl({
+      board: board.name,
+      port: port.comName,
+      debug: true
     });
+    var fs = require('fs');
+    fs.writeFile("data.hex", res, function(err) {
+      if (err) {
+        log("write file err")
+      } else {
+        avrgirl.flash("data.hex", function(error) {
+          if (error) {
+            console.error(error);
+          } else {
+            console.info('done.');
+          }
+          global.sender.send("u", (error ? false : true));
+        });
+      }
+    });
+  });
 });
 
 
 ipcMain.on('open', function(e, f) {
-    openFile(e);
+  openFile(e);
 });
 
 
 ipcMain.on('new', function() {
-    savedFile = null;
+  savedFile = null;
 })
 
 ipcMain.on('save', function(e, d) {
-    if (savedFile != null) {
-        fs.writeFile(savedFile, d, function(err) {});
-    } else {
-        saveFileB(d);
-    }
+  if (savedFile != null) {
+    fs.writeFile(savedFile, d, function(err) {});
+  } else {
+    saveFileB(d);
+  }
 });
 ipcMain.on('saveB', function(e, d) {
-    saveFileB(d);
+  saveFileB(d);
 });
 ipcMain.on('saveC', function(e, d) {
-    saveFileC(d);
+  saveFileC(d);
 });
 
 
 ipcMain.on('set', function() {
-    setWindow = new BrowserWindow({
-        width: 770,
-        height: 500,
-        minWidth: 770,
-        minHeight: 510,
-        frame: false,
-        show: false
-    });
-    setWindow.loadURL('file://' + __dirname + '/app/set.html');
-    setWindow.show();
-    setWindow.webContents.openDevTools();
-    ipcMain.on("setRun", function(_e, _d) {
-        setTimeout(() => {
-            _e.sender.send("setOk", require('./settings.json'));
-        }, 1000);
-    })
+  setWindow = new BrowserWindow({
+    width: 770,
+    height: 500,
+    minWidth: 770,
+    minHeight: 510,
+    frame: false,
+    show: false,
+    icon: "app/media/icons/settings.png",
+    title: "Settings"
+  });
+  setWindow.loadURL('file://' + __dirname + '/app/set.html');
+  setWindow.show();
+  if (require("./settings.json").debug) setWindow.webContents.openDevTools();
+  ipcMain.on("setRun", function(_e, _d) {
+    setTimeout(() => {
+      _e.sender.send("setOk", require('./settings.json'));
+    }, 1000);
+  })
 });
 ipcMain.on("updateSettings", function(e, d) {
-    fs.writeFileSync("settings.json", JSON.stringify(d));
+  fs.writeFile("settings.json", JSON.stringify(d), () => {
+    boards = get_boards();
+    global.sender.send('boardsRefresh', boards);
+    setWindow.hide();
+    app.relaunch({
+      args: process.argv.slice(1).concat(['--relaunch'])
+    })
+    app.exit(0)
+  });
 })
 
 ipcMain.on('boardSelected', function(e, d) {
-    board = d;
+  board = d;
 });
 
 ipcMain.on('portSelected', function(e, d) {
-    port = d;
-    global.sPort = d;
+  port = d;
+  global.sPort = d;
 });
 
 function log(e) {
-    if (require("./settings.json").debug) {
-        console.log(e);
-    }
+  if (require("./settings.json").debug) {
+    console.log(e);
+  }
 }
 
 /*
@@ -215,75 +231,93 @@ function log(e) {
  */
 
 function openFile(e) {
-    var { dialog } = require('electron');
-    dialog.showOpenDialog({
-        filters: [{ name: 'TechLab projects', extensions: ['tlab'] }]
-    }, function(fileNames) {
-        if (fileNames === undefined) return;
-        var fileName = fileNames[0];
-        savedFile = fileName;
-        fs.readFile(fileName, 'utf-8', function(err, data) {
-            e.sender.send('openOk', data);
-        });
+  var {
+    dialog
+  } = require('electron');
+  dialog.showOpenDialog({
+    filters: [{
+      name: 'TechLab projects',
+      extensions: ['tlab']
+    }]
+  }, function(fileNames) {
+    if (fileNames === undefined) return;
+    var fileName = fileNames[0];
+    savedFile = fileName;
+    fs.readFile(fileName, 'utf-8', function(err, data) {
+      e.sender.send('openOk', data);
     });
+  });
 }
 
 
 function saveFileB(d) {
-    var { dialog } = require('electron');
-    dialog.showSaveDialog({
-        filters: [{ name: 'TechLab projects', extensions: ['tlab'] }]
-    }, function(fileName) {
-        if (fileName === undefined) return;
-        savedFile = fileName;
-        fs.writeFile(fileName, d, function(err) {});
-    });
+  var {
+    dialog
+  } = require('electron');
+  dialog.showSaveDialog({
+    filters: [{
+      name: 'TechLab projects',
+      extensions: ['tlab']
+    }]
+  }, function(fileName) {
+    if (fileName === undefined) return;
+    savedFile = fileName;
+    fs.writeFile(fileName, d, function(err) {});
+  });
 }
 
 function saveFileC(d) {
-    var { dialog } = require('electron');
-    dialog.showSaveDialog({
-        filters: [{ name: 'Arduino sketch', extensions: ['ino'] }]
-    }, function(fileName) {
-        if (fileName === undefined) return;
-        fs.writeFile(fileName, d, function(err) {});
-    });
+  var {
+    dialog
+  } = require('electron');
+  dialog.showSaveDialog({
+    filters: [{
+      name: 'Arduino sketch',
+      extensions: ['ino']
+    }]
+  }, function(fileName) {
+    if (fileName === undefined) return;
+    fs.writeFile(fileName, d, function(err) {});
+  });
 }
 
 /*
  * Code processing
  */
 function get_hex(code, cb) {
-    var http = require('http');
-    var options = {
-        host: require("./settings.json").srv.url,
-        port: require("./settings.json").srv.port,
-        path: '/?data=' + encodeURI(JSON.stringify({ "board": board.aname, "sketch": code }))
-    };
+  var http = require('http');
+  var options = {
+    host: require("./settings.json").srv.url,
+    port: require("./settings.json").srv.port,
+    path: '/?data=' + encodeURI(JSON.stringify({
+      "board": board.aname,
+      "sketch": code
+    }))
+  };
 
-    callback = function(response) {
-        var str = '';
+  callback = function(response) {
+    var str = '';
 
-        response.on('data', function(chunk) {
-            str += chunk;
-        });
-        response.on('end', function() {
-            log('compilation finished');
-            var data = JSON.parse(str);
-            cb(data.hex, data.code);
-        });
-    }
+    response.on('data', function(chunk) {
+      str += chunk;
+    });
+    response.on('end', function() {
+      log('compilation finished');
+      var data = JSON.parse(str);
+      cb(data.hex, data.code);
+    });
+  }
 
-    http.request(options, callback).end();
-    log('compilation started');
+  http.request(options, callback).end();
+  log('compilation started');
 }
 
 function get_boards() {
-    var b = require('./boards.js');
-    if (onlyMainBoards) {
-        b = b.slice(0, 3);
-    }
-    return b;
+  var b = require('./boards.js');
+  if (require("./settings.json").onlyMainBoards) {
+    b = b.slice(0, 3);
+  }
+  return b;
 }
 
 /*
@@ -291,22 +325,25 @@ function get_boards() {
  */
 
 ipcMain.on("terminal", function(e, d) {
-    termWindow = new BrowserWindow({
-        width: 770,
-        height: 500,
-        minWidth: 770,
-        minHeight: 510,
-        frame: false,
-        show: false
-    });
-    termWindow.loadURL('file://' + __dirname + '/app/term.html');
-    termWindow.show();
-    termWindow.addListener("resize", function() {
-        e.sender.send("resized");
-    });
-    ipcMain.on("termRun", function(_e, _d) {
-        setTimeout(() => {
-            _e.sender.send("portOk", d);
-        }, 1500);
-    })
+  termWindow = new BrowserWindow({
+    width: 770,
+    height: 500,
+    minWidth: 770,
+    minHeight: 510,
+    frame: false,
+    show: false,
+    icon: "app/media/icons/terminal.png",
+    title: "Terminal"
+  });
+  termWindow.loadURL('file://' + __dirname + '/app/term.html');
+  termWindow.show();
+  if (require("./settings.json").debug) termWindow.webContents.openDevTools();
+  termWindow.addListener("resize", function() {
+    e.sender.send("resized");
+  });
+  ipcMain.on("termRun", function(_e, _d) {
+    setTimeout(() => {
+      _e.sender.send("portOk", d);
+    }, 900);
+  })
 });
