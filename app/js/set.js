9b8ed9810fa80cec54343357de59adbfabed0956
BardinPetr <bardin.petr@gmail.com>
2017-05-30 11:51:26 +0300

    Minor fixes

--- app/js/set.js
+++ app/js/set.js
@@ -1,5 +1,5 @@
 const {
-  ipcRenderer
+    ipcRenderer
 } = require('electron');
 var settings = {};
 
@@ -14,42 +14,42 @@ var ap = $("#ap");
 var apDiv = $("#apDiv");
 
 function log(e) {
-  console.log(e);
+    console.log(e);
 }
 
 $(function() {
-  ipcRenderer.send("setRun");
+    ipcRenderer.send("setRun");
 });
 
 $("#close").click(function() {
-  ipcRenderer.send('Sclose');
+    ipcRenderer.send('Sclose');
 });
 
 ipcRenderer.on("setOk", function(e, d) {
-  settings = d;
-  srvI.val(settings.srv.url);
-  portI.val(settings.srv.port);
-  ap.val(settings.arduinoPath);
-  (!settings.uploadType ? apDiv.removeClass("disabled") : apDiv.addClass("disabled"));
-  ombI.prop('checked', settings.onlyMainBoards);
-  dsspI.prop('checked', settings.disableSoftwarePorts);
-  dbgI.prop('checked', settings.debug);
-  upl.prop('checked', settings.uploadType);
-  thm.prop('checked', settings.theme);
+    settings = d;
+    srvI.val(settings.srv.url);
+    portI.val(settings.srv.port);
+    ap.val(settings.arduinoPath);
+    (!settings.uploadType ? apDiv.removeClass("disabled") : apDiv.addClass("disabled"));
+    ombI.prop('checked', settings.onlyMainBoards);
+    dsspI.prop('checked', settings.disableSoftwarePorts);
+    dbgI.prop('checked', settings.debug);
+    upl.prop('checked', settings.uploadType);
+    thm.prop('checked', settings.theme);
 });
 
 $("#sub").click(function() {
-  settings.srv.url = srvI.val();
-  settings.srv.port = parseInt(portI.val());
-  settings.arduinoPath = ap.val();
-  settings.onlyMainBoards = ombI.prop('checked');
-  settings.disableSoftwarePorts = dsspI.prop('checked');
-  settings.debug = dbgI.prop('checked');
-  settings.uploadType = upl.prop('checked');
-  settings.theme = thm.prop('checked');
-  ipcRenderer.send("updateSettings", settings);
+    settings.srv.url = srvI.val();
+    settings.srv.port = parseInt(portI.val());
+    settings.arduinoPath = ap.val();
+    settings.onlyMainBoards = ombI.prop('checked');
+    settings.disableSoftwarePorts = dsspI.prop('checked');
+    settings.debug = dbgI.prop('checked');
+    settings.uploadType = upl.prop('checked');
+    settings.theme = thm.prop('checked');
+    ipcRenderer.send("updateSettings", settings);
 });
 
-$(".can-toggle__switch").click(function() {
-  (upl.prop('checked') ? apDiv.removeClass("disabled") : apDiv.addClass("disabled"));
+$(".upl").click(function() {
+    (upl.prop('checked') ? apDiv.removeClass("disabled") : apDiv.addClass("disabled"));
 });
