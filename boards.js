ec81d18292f119db254f94ca389fc28a9f06cb3c
BardinPetr <bardin.petr@gmail.com>
2017-06-02 11:08:26 +0300

    Fixed boards list

--- boards.js
+++ boards.js
@@ -20,8 +20,6 @@ var boards = [{
         protocol: 'stk500v1',
         aname: 'arduino:avr:nano:cpu=atmega328'
     },
-
-    //TODO: set avrdude names of boards
     {
         name: 'mega',
         baud: 115200,
@@ -38,7 +36,7 @@ var boards = [{
         pollIndex: 0x03,
         productId: ['0x0042', '0x6001', '0x0010', '0x7523'],
         protocol: 'stk500v2',
-        aname: 'arduino:avr:uno'
+        aname: 'arduino:avr:mega:cpu=atmega2560'
     },
 
     {
@@ -47,15 +45,7 @@ var boards = [{
         signature: new Buffer([0x43, 0x41, 0x54, 0x45, 0x52, 0x49, 0x4e]),
         productId: ['0x0037', '0x8037', '0x0036', '0x0237'],
         protocol: 'avr109',
-        aname: 'arduino:avr:uno'
-    },
-    {
-        name: 'imuduino',
-        baud: 57600,
-        signature: new Buffer([0x43, 0x41, 0x54, 0x45, 0x52, 0x49, 0x4e]),
-        productId: ['0x0036', '0x8037', '0x8036'],
-        protocol: 'avr109',
-        aname: 'arduino:avr:uno'
+        aname: 'arduino:avr:micro'
     },
     {
         name: 'leonardo',
@@ -63,23 +53,7 @@ var boards = [{
         signature: new Buffer([0x43, 0x41, 0x54, 0x45, 0x52, 0x49, 0x4e]),
         productId: ['0x0036', '0x8036', '0x800c'],
         protocol: 'avr109',
-        aname: 'arduino:avr:uno'
-    },
-    {
-        name: 'arduboy',
-        baud: 57600,
-        signature: new Buffer([0x43, 0x41, 0x54, 0x45, 0x52, 0x49, 0x4e]),
-        productId: ['0x0036', '0x8036', '0x800c'],
-        protocol: 'avr109',
-        aname: 'arduino:avr:uno'
-    },
-    {
-        name: 'feather',
-        baud: 57600,
-        signature: new Buffer([0x43, 0x41, 0x54, 0x45, 0x52, 0x49, 0x4e]),
-        productId: ['0x800c', '0x000c'],
-        protocol: 'avr109',
-        aname: 'arduino:avr:uno'
+        aname: 'arduino:avr:leonardo'
     },
     {
         name: 'little-bits',
@@ -87,15 +61,7 @@ var boards = [{
         signature: new Buffer([0x43, 0x41, 0x54, 0x45, 0x52, 0x49, 0x4e]),
         productId: ['0x0036', '0x8036'],
         protocol: 'avr109',
-        aname: 'arduino:avr:uno'
-    },
-    {
-        name: 'blend-micro',
-        baud: 57600,
-        signature: new Buffer([0x43, 0x41, 0x54, 0x45, 0x52, 0x49, 0x4e]),
-        productId: ['0x2404'],
-        protocol: 'avr109',
-        aname: 'arduino:avr:uno'
+        aname: 'arduino:avr:nano:cpu=atmega328'
     },
     {
         name: 'duemilanove168',
@@ -106,29 +72,7 @@ var boards = [{
         timeout: 400,
         productId: ['0x6001'],
         protocol: 'stk500v1',
-        aname: 'arduino:avr:uno'
-    },
-    {
-        name: 'tinduino',
-        baud: 57600,
-        signature: new Buffer([0x1e, 0x95, 0x0f]),
-        pageSize: 128,
-        numPages: 256,
-        timeout: 400,
-        productId: ['0x6015'],
-        protocol: 'stk500v1',
-        aname: 'arduino:avr:uno'
-    },
-    {
-        name: 'bqZum',
-        baud: 19200,
-        signature: new Buffer([0x1e, 0x95, 0x0f]),
-        pageSize: 128,
-        numPages: 256,
-        timeout: 400,
-        productId: ['0x6001', '0x7523'],
-        protocol: 'stk500v1',
-        aname: 'arduino:avr:uno'
+        aname: 'arduino:avr:diecimila:cpu=atmega168'
     },
     {
         name: 'adk',
@@ -146,68 +90,8 @@ var boards = [{
         pollIndex: 0x03,
         productId: ['0x0044', '0x6001', '0x003F'],
         protocol: 'stk500v2',
-        aname: 'arduino:avr:uno'
-    },
-    {
-        name: 'sf-pro-micro',
-        baud: 57600,
-        signature: new Buffer([0x43, 0x41, 0x54, 0x45, 0x52, 0x49, 0x4e]),
-        productId: ['0x9206'],
-        protocol: 'avr109',
-        aname: 'arduino:avr:uno'
-    },
-    {
-        name: 'pro-mini',
-        baud: 57600,
-        signature: new Buffer([0x1e, 0x95, 0x0f]),
-        pageSize: 128,
-        numPages: 256,
-        timeout: 400,
-        protocol: 'stk500v1',
-        aname: 'arduino:avr:uno'
-    },
-    {
-        name: 'qduino',
-        baud: 57600,
-        signature: new Buffer([0x43, 0x41, 0x54, 0x45, 0x52, 0x49, 0x4e]),
-        productId: ['0x516d'],
-        protocol: 'avr109',
-        aname: 'arduino:avr:uno'
-    },
-    {
-        name: 'pinoccio',
-        baud: 115200,
-        signature: new Buffer([0x1e, 0xa8, 0x02]), // ATmega256RFR2
-        pageSize: 256,
-        delay1: 10,
-        delay2: 1,
-        timeout: 0xc8,
-        stabDelay: 0x64,
-        cmdexeDelay: 0x19,
-        synchLoops: 0x20,
-        byteDelay: 0x00,
-        pollValue: 0x53,
-        pollIndex: 0x03,
-        productId: ['0x6051'],
-        protocol: 'stk500v2',
-        aname: 'arduino:avr:uno'
-    },
-    {
-        name: 'lilypad-usb',
-        baud: 57600,
-        signature: new Buffer([0x43, 0x41, 0x54, 0x45, 0x52, 0x49, 0x4e]),
-        productId: ['0x9207', '0x9208', '0x1B4F'],
-        protocol: 'avr109',
-        aname: 'arduino:avr:uno'
-    },
-    {
-        name: 'yun',
-        baud: 57600,
-        signature: new Buffer([0x43, 0x41, 0x54, 0x45, 0x52, 0x49, 0x4e]),
-        productId: ['0x0041', '0x8041'],
-        protocol: 'avr109',
-        aname: 'arduino:avr:uno'
-    },
+        aname: 'arduino:avr:megaADK'
+    }
 ];
 
 module.exports = boards;
